737dfe74242ce5a226fa44473f79d2ea109f7daf
Colin Brown <cbrown12@umassd.edu>
2018-10-19 13:07:59 -0400

    Remove print

--- files2json.py
+++ files2json.py
@@ -3,7 +3,6 @@ import json
 d_obj = {}
 for file in os.listdir('./data'):
     with open('./data/'+file,'r') as f:
-        print(file)
         d_obj[file] = f.read()
 with open('data.json', 'w') as outfile:
     json.dump(d_obj, outfile)
