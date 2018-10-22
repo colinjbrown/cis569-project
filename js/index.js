1ff94916153ab49be1d5bc4f413a19232d598d49
Unknown <605266076@qq.com>
2018-10-22 19:45:44 -0400

    Add some commons in the js file.

--- js/index.js
+++ js/index.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
 
         doc_list
             .enter()
+            //Creat document id in the list for each document.
             .append('li')
             .append('p').attr('id',function (d) {
                 return d.replace('.','')+'-li';
@@ -23,16 +24,20 @@ $(document).ready(function(){
             .classed('list-group-item',true)
             .html(function (d) {
                 return d;
-            }).on('mouseover', function () {
+            })
+            //let the ID highlight when the mouse on it.
+            .on('mouseover', function () {
             $(this).toggleClass("hover", true);
             })
             .on('mouseout', function () {
                 $(this).toggleClass("hover", false);
             })
+            //display or delete the document in the workplace by click the document ID.
             .on('click', function(d)
                 {
+                    //if the document item don't be displaied, display it.
                     if($("#" + d.replace('.','')).length == 0) {
-
+                        //try to creat the document in the workplace.
                         doc_div = d3.select('#wp').append('div').classed('drag',true).classed('card',true).attr('id',function (){return d.replace('.','');});
 
                         doc_div.append('h3').html(d).classed('card-header',true).classed('primary-color',true).classed('white-text',true).classed('label',true);
@@ -40,13 +45,14 @@ $(document).ready(function(){
                         doc_div.append('div').classed('card-body',true).html(function () {
                             return data[d];
                         })
-
+                        //let the document item can be draggable in the workplace, and user can delete it by double click.
                         $('#'+d.replace('.','')).unbind('draggable').draggable({stack:'.drag',containment: "parent"}).dblclick(function(){
                             $(this).remove();
-                            $('#'+d.replace('.','')+'-li').toggleClass('active');
+                            $('#'+d.replace('.','')+'-li').toggleClass('active'); //Change the color of the ID box.
                             //$("#p1").css("background-color", "#aaa");
                         });
                     }
+                    //if the document has been displaied, delete it.
                     else{
                         $('#'+d.replace('.','')).remove();
                     }
@@ -56,7 +62,7 @@ $(document).ready(function(){
         ;
 
     //$( ".drag" ).unbind("draggable").draggable({ containment: "parent"});
-
+        //let the IDs in the list can be ordered by mouse.
         $( "#sortable" ).sortable();
         $( "#sortable" ).disableSelection();
 
