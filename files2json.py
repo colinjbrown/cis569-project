623bc4ffade8bc29dbffaef492b4faaed9ef2bf0
Colin Brown <cbrown12@umassd.edu>
2018-11-14 17:02:48 -0500

    Add code for creating a vectorized representation of documents

--- files2json.py
+++ files2json.py
@@ -1,8 +1,33 @@
 import os
 import json
+import nltk
+import string
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.manifold import TSNE
+
+# Download punkt if needed
+# nltk.download('punkt')
+
 d_obj = {}
 for file in os.listdir('./data'):
-    with open('./data/'+file,'r') as f:
+    with open('./data/' + file, 'r') as f:
         d_obj[file] = f.read()
-with open('data.json', 'w') as outfile:
-    json.dump(d_obj, outfile)
+
+# Use this to produce the original data.json
+# with open('data.json', 'w') as outfile:
+#     json.dump(d_obj, outfile)
+
+df = pd.DataFrame.from_dict(d_obj, orient='index', columns=['Text'])
+v = TfidfVectorizer()
+x = v.fit_transform(df['Text'])
+
+# TSNE is good for visualization of high dimensional data
+# It's primarily used for dimensionality reduction in place of methods like PCA
+# Please see https://lvdmaaten.github.io/tsne/ for more information
+tsne_reduced = TSNE(n_components=2).fit_transform(x.toarray())
+df['tsne0'] = tsne_reduced[:, 0]
+df['tsne1'] = tsne_reduced[:, 1]
+
+# Finally output our new dataset
+df.to_json('reduced-data.json', orient='index')
